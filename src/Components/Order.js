0564f821dcbbb1f046a16aebc2e444979042cd78
Arina Gasanly <Arinakudinova111@yandex.ru>
2022-04-05 22:29:41 +0300

    Order List

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ButtonCheckout } from './ButtonCheckout';
+import { OrderListItem } from './OrderListItem';
 
 const OrderStyled = styled.section`
  position: fixed;
@@ -11,13 +12,14 @@ const OrderStyled = styled.section`
  background: #fff;
  min-width: 380px;
  heigth: calc(100% - 80px);
- box-shadow: 3px 4px 5pxx rgba(0, 0, 0, 0.25);
+ box-shadow: 3px 4px 5px rgba(0, 0, 0, 0.25);
  padding: 20px;
 `;
 
 
 const OrderTitle = styled.h2`
  text-align: center;
+ margin-bottom: 30px;
 `;
 
 const OrderContent = styled.div`
@@ -25,14 +27,23 @@ flex-grow: 1;
 `;
 
 
-const OrderList = styled.ul``;
+const OrderList = styled.ul`
+
+`;
 
 
 const Total = styled.div`
 display: flex;
+margin: 0 35px 30px;
 & span:first-child {
   flex-grow: 1;
 }
+`;
+
+const TotalPrice = styled.span`
+text-align: right;
+min-width: 65px;
+margin-left: 20px;
 `;
 
 export const Order = () => {
@@ -41,13 +52,15 @@ export const Order = () => {
     <OrderTitle>ВАШ ЗАКАЗ</OrderTitle>
     <OrderContent>
       <OrderList>
-
+       <OrderListItem/>
+       <OrderListItem/>
+       <OrderListItem/>
       </OrderList>
     </OrderContent>
     <Total>
       <span>Итого</span>
       <span>5</span>
-      <span>850 Р</span>
+      <TotalPrice>850 Р</TotalPrice>
     </Total>
     <ButtonCheckout>Оформить</ButtonCheckout>
     </OrderStyled>
--- src/Components/OrderListItem.js
+++ src/Components/OrderListItem.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import trashImage from '../image/trash.svg';
 
 
-export const OrderListItem = () => {
 
+const OrderItemStyled = styled.li`
+ display: flex;
+ margin: 15px 0;
+`;
 
-};
+const ItemName = styled.span`
+ flex-grow: 1;
+`;
+
+const ItemPrice = styled.span`
+ margin-left: 20px;
+ margin-right: 10px;
+ margin-width: 65px;
+ text-align: right;
+`;
+
+const TrashButton = styled.button`
+ width: 24px;
+ height: 24px;
+ border-color: transparent;
+ background-color: transparent;
+ background-image: url(${trashImage});
+ background-position: center;
+ background-size: cover;
+ background-repeat: no-repeat;
+ cursor: pointer;
+`;
+
+export const OrderListItem = () => (
+<OrderItemStyled>
+  <ItemName>JS Burger</ItemName>
+  <span>2</span>
+  <ItemPrice>750 P</ItemPrice>
+  <TrashButton/>
+</OrderItemStyled>
+
+);
